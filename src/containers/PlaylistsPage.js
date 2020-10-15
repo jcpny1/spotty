0e2686e0114d6164b6f74de38d562c6fe9e70c06
jcpny1 <jpfingst@outlook.com>
2020-10-14 22:39:33 -0400

    Only update display once all playlists to be displayed are loaded.

--- src/containers/PlaylistsPage.js
+++ src/containers/PlaylistsPage.js
@@ -13,7 +13,9 @@ export default class PlaylistsPage extends Component {
       activeTrackList: null,
       playlists:       null,   // Spotify playlists response
       sortDirection:   '',
-      sortColumn: '',
+      sortColumn:      '',
+      listCombine:     null,
+      responseCount:   0,
     };
   }
 
@@ -57,6 +59,7 @@ export default class PlaylistsPage extends Component {
         });
       } else if (index === (playlistsItems.length - 1)) {
         // ALL TRACKS
+        this.setState({responseCount: 0, listCombine: {items:[]}});
         for (var i = 0; i < (playlistsItems.length - 2); i++) {
           // All playlists
           $.ajax({
@@ -66,8 +69,10 @@ export default class PlaylistsPage extends Component {
               xhr.setRequestHeader("Authorization", "Bearer " + this.props.accessToken);
             },
             success: (data) => {
+              this.setState({responseCount: this.state.responseCount + 1});
+
               if (this.state.activeIndex === (playlistsItems.length - 1)) {  // are we still servicing this request?
-                var xxx = this.state.activeTrackList;
+                var atl = this.state.listCombine;
 
                 // convert playlist id to playlist name.
                 const playlistId = data.href.split('/')[5];  // get playlist id
@@ -75,12 +80,16 @@ export default class PlaylistsPage extends Component {
 
                 for (var j = 0; j < data.items.length; j++) {
                   data.items[j].playlistName = playlist.name; // add playlist name as property to each item.
-                  xxx.items.push(data.items[j]);
+                  atl.items.push(data.items[j]);
                 }
 
-                this.sortActiveTrackList('track.name', 'a');
-                this.flagDuplicates(xxx);
-                this.setState({activeTrackList: xxx});
+                if (this.state.responseCount < (playlistsItems.length - 1)) {
+                  this.setState({listCombine: atl});
+                } else {
+                  this.sortTrackList(atl, 'track.name', 'a');
+                  this.flagDuplicates(atl);
+                  this.setState({activeTrackList: atl});
+                }
               }
             }
           });
@@ -93,17 +102,23 @@ export default class PlaylistsPage extends Component {
             xhr.setRequestHeader("Authorization", "Bearer " + this.props.accessToken);
           },
           success: (data) => {
+            this.setState({responseCount: this.state.responseCount + 1});
+
             if (this.state.activeIndex === (playlistsItems.length - 1)) {  // are we still servicing this request?
-              var atl = this.state.activeTrackList;
+              var atl = this.state.listCombine;
 
               for (var j = 0; j < data.items.length; j++) {
                 data.items[j].playlistName = 'LIKED'; // add playlist name as property to each item.
                 atl.items.push(data.items[j]);
               }
 
-              this.sortActiveTrackList('track.name', 'a');
-              this.flagDuplicates(atl);
-              this.setState({activeTrackList: atl});
+              if (this.state.responseCount < (playlistsItems.length - 1)) {
+                this.setState({listCombine: atl});
+              } else {
+                this.sortTrackList(atl, 'track.name', 'a');
+                this.flagDuplicates(atl);
+                this.setState({activeTrackList: atl});
+              }
             }
           }
         });
@@ -124,48 +139,55 @@ export default class PlaylistsPage extends Component {
     atl.items = _.flatMapDepth(counts, null, 2);
   }
 
-// clean this up a bit.
   sortActiveTrackList = (columnName, direction = null) => {
-      var data = this.state.activeTrackList;
-      var dir = direction;
+    this.setState({activeTrackList: this.sortTrackList(this.state.activeTrackList, columnName, direction)});
+  }
 
-      if (dir === null) {
-        dir = (this.state.sortColumn !== columnName) ? 'a' : (this.state.sortDirection === 'a') ? 'd' : 'a';
-      }
+// make specific, because columnName could be same, but table could be different. OR maybe store data in table instead of state.
+// clean this up a bit.
+  sortTrackList = (data, columnName, direction = null) => {
+    var dir = direction;
 
-      data.items = data.items.sort(function (item1, item2) {
-        // item1 = eval('item1.' + columnName);
-        // item2 = eval('item2.' + columnName);
-        item1 = _.get(item1, columnName);
-        item2 = _.get(item2, columnName);
+    if (dir === null) {
+      dir = (this.state.sortColumn !== columnName) ? 'a' : (this.state.sortDirection === 'a') ? 'd' : 'a';
+    }
 
-        if (typeof item1 === 'string') {
-          if (dir === 'a') {
-            return item1.localeCompare(item2, 'en', { sensitivity: 'base', numeric: true, ignorePunctuation: true });
-          } else {
-            return item2.localeCompare(item1, 'en', { sensitivity: 'base', numeric: true, ignorePunctuation: true });
-          }
+    this.setState({sortColumn: columnName, sortDirection: dir});
+
+    data.items = data.items.sort(function (item1, item2) {
+      // item1 = eval('item1.' + columnName);
+      // item2 = eval('item2.' + columnName);
+      item1 = _.get(item1, columnName);
+      item2 = _.get(item2, columnName);
+
+      if (typeof item1 === 'string') {
+        if (dir === 'a') {
+          return item1.localeCompare(item2, 'en', { sensitivity: 'base', numeric: true, ignorePunctuation: true });
         } else {
-          if (dir === 'a') {
-            if (item1 < item2) {
-              return -1;
-            }
-            if (item1 > item2) {
-              return 1;
-            }
-            return 0;
-          } else {
-            if (item1 < item2) {
-              return 1;
-            }
-            if (item1 > item2) {
-              return -1;
-            }
-            return 0;
-          }
+          return item2.localeCompare(item1, 'en', { sensitivity: 'base', numeric: true, ignorePunctuation: true });
         }
-      });
-      this.setState({activeTrackList: data, sortColumn: columnName, sortDirection: dir});
+      } else {
+        if (dir === 'a') {
+          if (item1 < item2) {
+            return -1;
+          }
+          if (item1 > item2) {
+            return 1;
+          }
+          return 0;
+        } else {
+          if (item1 < item2) {
+            return 1;
+          }
+          if (item1 > item2) {
+            return -1;
+          }
+          return 0;
+        }
+      }
+    });
+
+    return data;
   }
 
   handleClick = (e, playlistProps) => {
