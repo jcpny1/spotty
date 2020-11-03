b54949c84f9cebd2416f1f5b4554d557ed291499
jcpny1 <jpfingst@outlook.com>
2020-11-03 18:28:01 -0500

    Change login link to a Button.

--- client/src/containers/LoginPage.js
+++ client/src/containers/LoginPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Button} from 'semantic-ui-react';
 
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientId     = process.env.REACT_APP_CLIENT_ID;
@@ -8,12 +9,7 @@ const scopes       = 'user-library-read user-read-email user-read-private';
 export default class LoginPage extends Component {
   render() {
     return (
-      <a
-        className='btn btn--loginApp-link'
-        href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes}&response_type=code&show_dialog=false`}
-      >
-        Login
-      </a>
+      <Button as='a' href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes}&response_type=code&show_dialog=false`} content='Login' title='Connect to Spotify' inverted size='medium' />
     );
   }
 }
