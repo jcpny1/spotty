843395c09d9ade83b9ef4b051a0aa5303613dd16
jcpny1 <jpfingst@outlook.com>
2021-01-22 23:19:50 -0500

    Remove bazillion travis-ci messages 'The node type SpreadProperty has been renamed to SpreadElement'.

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,5 +1,5 @@
 // babel.config.js
 module.exports = {
   presets: [['@babel/preset-env', {targets: {node: 'current'}}], "@babel/preset-react"],
-  plugins: ["transform-class-properties", "transform-object-rest-spread", "@babel/plugin-proposal-object-rest-spread"],
+  plugins: ["transform-class-properties", "@babel/plugin-proposal-object-rest-spread"],
 };
